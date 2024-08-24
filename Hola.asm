9444b3f97ca6b59161c64859d739a76a958b7acc
Santino <santinopiantini@gmail.com>
2024-08-24 05:00:03 -0300

    Hola mundo en assembler

--- Hola.asm
+++ Hola.asm
@@ -1,9 +1,9 @@
-.model small
-.stack
-.data
+.model small ;Declaracion del programa 
+.stack ;Segmento de pila (1024) por defecto
+.data ;Declaracion de las variables
 	msg db "Hello world Curso Assembler!!!$" ;msg es el nombre de la variable y db es el tipo de dato
 
-.code
+.code ;Todo el codigo y sus metodos 
 
 	main PROC
 		;carga de segmento de datos a segmento de codigo
